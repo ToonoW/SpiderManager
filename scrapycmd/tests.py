01ece4ac34c0ef2821cc3b7f401595388c20e69d
伍锦文 <toono@qq.com>
2017-09-01 14:16:42 +0800

    1. 添加查看所有project爬虫运行记录功能及其单元测试； 2. 添加运行和暂停单一爬虫功能及其单元测试； 3. 添加依赖库列表。

--- scrapycmd/tests.py
+++ scrapycmd/tests.py
@@ -1,27 +1,54 @@
 from django.test import TestCase
 
 from scrapycmd.utils import scrapycmd
-from scrapyinfo.tests import ScraoyInfoTest
-from scrapyinfo import models as scrapyinfo_models
+from scrapyinfo.tests import ScraoyInfoTest, ScrapydModelTest
+from scrapyinfo import models as info_models
+from scrapyinfo.utils.scrapyinfo import refresh_all_project_and_scrapy
+
 
 class ScrapycmdTest(TestCase):
 
-    # def test_run_spider(self):
-    #     """
-    #     测试运行单个爬虫
-    #     """
-    #     scrapycmd.run_spider(spider)
+    def test_run_and_cancel_spider(self):
+        """
+        运行单个爬虫然后取消运行计划
+        """
+        test = ScraoyInfoTest()
+        test.test_fetch_projects()
+        refresh_all_project_and_scrapy()
 
-    def test_query_project_spider_log(self):
+        spider = info_models.Spider.objects.last()
+        job = scrapycmd.run_spider(spider)
+        print(job)
+
+        status = scrapycmd.cancel_spider(spider, job['jobid'])
+        self.assertEqual(status['status'], 'ok')
+
+    def test_query_project_spiders_log(self):
         """
         查看某一project的爬虫运行记录
         """
         test = ScraoyInfoTest()
         test.test_fetch_projects()
         test.test_update_projects()
-        project = scrapyinfo_models.Project.objects.last()
+        project = info_models.Project.objects.last()
 
         status_dic = scrapycmd.query_spiders_log(project)
         self.assertTrue(len(status_dic['pending']) >= 0)
         self.assertTrue(len(status_dic['running']) >= 0)
         self.assertTrue(len(status_dic['finished']) >= 0)
+
+    def test_query_all_project_spiders_log(self):
+        """
+        查看所有project的爬虫运行记录
+        """
+        test = ScraoyInfoTest()
+        test.test_fetch_projects()
+        test.test_update_projects()
+
+        status_list = scrapycmd.query_all_spiders_log()
+        for status in status_list:
+            self.assertTrue(status['project_id'] is not None)
+            self.assertTrue(len(status['pending']) >= 0)
+            self.assertTrue(len(status['running']) >= 0)
+            self.assertTrue(len(status['finished']) >= 0)
+
--- scrapycmd/utils/scrapycmd.py
+++ scrapycmd/utils/scrapycmd.py
@@ -1,20 +1,86 @@
 import requests as rq
 
+from scrapyinfo import models as info_models
+
+
+def run_spider(spider):
+    """
+    运行一个爬虫
+    :param spider:爬虫
+    :return: {"status": "ok", "jobid": "6487ec79947edab326d6db28a2d86511e8247444"}
+    """
+    scrapyd = spider.project.scrapyd
+    url = 'http://{}:{}/schedule.json'.format(scrapyd.ip, scrapyd.port)
+    response = rq.post(url, data={
+        'project': spider.project.name,
+        'spider': spider.name,
+    })
+    data = response.json()
+    status = data.get('status')
+    if status != 'ok':
+        raise ValueError('请求参数错误')
+    else:
+        return data
+
+
+def cancel_spider(spider, job_id):
+    """
+    取消一个爬虫
+    :param job_id: 任务ID
+    """
+    scrapyd = spider.project.scrapyd
+    url = 'http://{}:{}/cancel.json'.format(scrapyd.ip, scrapyd.port)
+    response = rq.post(url, data={
+        'project': spider.project.name,
+        'job': job_id,
+    })
+    data = response.json()
+    status = data.get('status')
+    if status != 'ok':
+        print(data)
+        raise ValueError('请求参数错误')
+    else:
+        return data
+
 
 def query_spiders_log(project):
     """
     查看某一project下的爬虫运行记录
     :param project: 需要查询记录的project
+    :return:
+                {"status": "ok",
+                 "pending": [{"id": "78391cc0fcaf11e1b0090800272a6d06", "spider": "spider1"}],
+                 "running": [{"id": "422e608f9f28cef127b3d5ef93fe9399", "spider": "spider2", "start_time": "2012-09-12 10:14:03.594664"}],
+                 "finished": [{"id": "2f16646cfcaf11e1b0090800272a6d06", "spider": "spider3", "start_time": "2012-09-12 10:14:03.594664", "end_time": "2012-09-12 10:24:03.594664"}]}
     """
     url = 'http://{}:{}/listjobs.json?project={}'.format(project.scrapyd.ip,
-                                           project.scrapyd.port,
-                                           project.name,
-                                           )
+                                                         project.scrapyd.port,
+                                                         project.name,
+                                                         )
     response = rq.get(url)
     data = response.json()
     status = data.get('status')
-    print(status)
     if status != 'ok':
         raise ValueError('请求参数错误')
     else:
         return data
+
+
+def query_all_spiders_log():
+    """
+    查看所有
+    :return:
+                {"project_id": 1,
+                 "status": "ok",
+                 "pending": [{"id": "78391cc0fcaf11e1b0090800272a6d06", "spider": "spider1"}],
+                 "running": [{"id": "422e608f9f28cef127b3d5ef93fe9399", "spider": "spider2", "start_time": "2012-09-12 10:14:03.594664"}],
+                 "finished": [{"id": "2f16646cfcaf11e1b0090800272a6d06", "spider": "spider3", "start_time": "2012-09-12 10:14:03.594664", "end_time": "2012-09-12 10:24:03.594664"}]}
+    """
+    projects = info_models.Project.objects.all()
+
+    status_list = list()
+    for project in projects:
+        status = query_spiders_log(project)
+        status['project_id'] = project.id
+        status_list.append(status)
+    return status_list
